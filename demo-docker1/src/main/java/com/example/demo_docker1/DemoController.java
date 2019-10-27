3e7af42ccb883f015ba2d25545ba50799eefccaf
FANDROIDE <codice.f.m.s@gmail.com>
2019-10-26 21:00:49 -0500

    se agrega proyecto

---
+++ demo-docker1/src/main/java/com/example/demo_docker1/DemoController.java
@@ -0,0 +1,31 @@
+package com.example.demo_docker1;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class DemoController
+{
+    @RequestMapping("/")
+    public List<Customer> findAll()
+    {
+        List<Customer> customerList = new ArrayList<Customer>();
+        customerList.add(new Customer(1, "frank"));
+        customerList.add(new Customer(2, "john"));
+        customerList.add(new Customer(3, "Feliciano"));
+        return customerList;
+    }
+    @RequestMapping("/saludar")
+    public List<Customer> saludo()
+    {
+        List<Customer> customerList = new ArrayList<Customer>();
+        customerList.add(new Customer(1, "ola"));
+        customerList.add(new Customer(2, "k"));
+        customerList.add(new Customer(3, "ace"));
+        customerList.add(new Customer(3, "nuevaversion"));
+        return customerList;
+    }
+}
